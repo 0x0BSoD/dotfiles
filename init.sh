e6306e09b7f90782faa7ca76958dd145898fccf1
Alexander Simonov <zlodey23@gmail.com>
2018-06-25 18:23:13 +0300

    change init script

--- init.sh
+++ init.sh
@@ -1,7 +1,51 @@
-#!/bin/sh
-ln -s ./zshrc     ~/.zshrc
-ln -s ./vimrc     ~/.vimrc
-ln -s ./tmux.conf ~/.tmux.conf
-ln -s ./i3config/config ~/.config/i3/config
-ln -s ./scripts ~/.scripts
-ln -s ./polybar ~/.polybar
+#!/bin/bash
+
+
+home_configs=('zshrc'
+              'vimrc'
+              'tmux.conf'
+              'scripts')
+
+
+function make_link () {
+    if ! $2; then
+        ln -s "$PWD/$1" "$HOME/.$1"
+    else
+        ln -s "$PWD/$1" "$HOME/.config/$1"
+    fi
+}
+
+
+function start_this() {
+    for c in ${home_configs[@]}; do
+        echo "$HOME/$c"
+        if [ -f "$HOME/.$c" ]; then
+            echo "$c -- File removed!"
+            rm "$HOME/.$c"
+        fi
+        if [ -L "$HOME/.$c" ]; then
+            echo "$c -- Link removed!"
+            rm "$HOME/.$c"
+        fi
+        if [ -d "$HOME/.$c" ]; then
+            echo "$c -- Dir removed!"
+            rm -Rf "$HOME/.$c"
+        fi
+        make_link $c false
+    done
+}
+
+dialog --title "Config pusher" --yesno "Your existing configs has be delited. Do you wish to begin?" 7 60
+response=$?
+clear
+case $response in
+    0) start_this ;;
+    1) exit ;;
+esac
+
+#ln -s ./zshrc     ~/.zshrc
+#ln -s ./vimrc     ~/.vimrc
+#ln -s ./tmux.conf ~/.tmux.conf
+#ln -s ./i3/config ~/.config/i3/config
+#ln -s ./scripts   ~/.scripts
+#ln -s ./polybar   ~/.config/polybar
